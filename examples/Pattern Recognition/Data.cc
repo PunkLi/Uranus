f3be89ea5c5b437bd720e4720dd44029b1fe8103
chunpeng <lichunpeng2200@outlook.com>
2018-10-18 11:13:34 +0800

    add Fisher-UCI-Sonar example.

--- examples/Pattern Recognition/Data.cc
+++ examples/Pattern Recognition/Data.cc
@@ -10,6 +10,14 @@
 #include <string>
 #include <vector>
 
+#include "uranus/Matrix.hpp"
+
+using DataSet = std::vector<
+					std::pair<std::vector<double>,
+							  std::string
+				    	>
+					>;
+
 // 留着生成随机数，做交叉验证
 int uniform_intx(int a, int b) {
 	static std::default_random_engine e{ std::random_device{}() };
@@ -17,21 +25,21 @@ int uniform_intx(int a, int b) {
 	return u(e, std::uniform_int_distribution<int>::param_type(a, b));
 }
 
-double Trim(std::string& str)
+std::string Trim(std::string& str)
 {
 	str.erase(0, str.find_first_not_of(" \t\r\n"));
 	str.erase(str.find_last_not_of(" \t\r\n") + 1);
-	return std::stod(str);
+	return str;
 }
-
 /**	
  * @brief read data set from file ,such as *.csv & *.txt & *.data
  * @param file_name
- * @param number of feature
- * @param number of sample set
- * @param number of class, default as 2
+ * @param feature_vec of feature
+ * @param feature_vec of sample set
+ * @param feature_vec of class, default as 2
  */
-void readData(std::string& file_name, 
+void readData(DataSet& buffer,
+			  std::string& file_name, 
               int feature_num, 
               int sample_num,
               int class_num = 2)
@@ -39,7 +47,7 @@ void readData(std::string& file_name,
 	std::ifstream fin(file_name);
 	std::string line;
 	
-	while (sample_num--)
+	for (int index = 0; index < sample_num; ++index)
 	{
 		std::getline(fin, line);
 		std::istringstream sin(line);
@@ -49,24 +57,52 @@ void readData(std::string& file_name,
 			fields.push_back(field);
 
 		int idx = feature_num;
-		std::vector<double> number(fields.size());
+		std::vector<double> feature_vec(fields.size());
 		for (int idx = 0; idx < feature_num; ++idx)
-			number[idx] = Trim(fields[idx]);
+			feature_vec[idx] = std::stod(Trim(fields[idx]));
+		std::string label = Trim(fields[idx]);
 
+		buffer[index] = std::make_pair(feature_vec,label);
+		/*
 		std::cout << "处理之后的字符串：" 
-			<< number[0] << "\t" 
-			<< number[1] << "\t"
-			<< number[2] << "\t"
-			<< number[3] << "\t"
+			<< feature_vec[0] << "\t" 
+			<< feature_vec[1] << "\t"
+			<< feature_vec[2] << "\t"
+			<< feature_vec[3] << "\t"
 			<< std::endl;
-
+		*/
 		// To-do: 还需要多读一个label，还需要封装
 	}
 }
 
 int main(int argc, char *argv[])
 {	
+	constexpr int feature = 4;
+	constexpr int sample_num = 150;
+
+	DataSet buffer(sample_num);
+
 	std::string name = "iris.data";
-	readData(name, 4, 150);
+	readData(buffer, name, feature, sample_num);
+
+	// To-do : std::vector -> uranus::vector , step by step 
+	/*
+	std::vector<std::vector<uranus::Vector<feature>>> x_set;
+
+	int class_id;
+
+	for(int j = 0; j < feature; ++j)
+		x_set[class_id][0](j) = buffer[0].first[j];
+		
+	for(int i = 1; i < sample_num; ++i)
+	{
+		for(int j = 0; j < feature; ++j)
+		{
+			if(buffer[i].second != buffer[i-1].second) class_id++;
+			x_set[class_id][i](j) = buffer[i].first[j];
+			// 第i类 / 类内 / 逐元素    第i类  / vector / 逐元素
+		}
+	}
+	*/
 	return EXIT_SUCCESS;
 }
